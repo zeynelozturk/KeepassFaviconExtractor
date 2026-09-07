732100ffdf27d3c81fec2fd6b13fe5fa2aa615e5
Zeynel (Desktop) <zeynel.ozturk@gmail.com>
2026-09-08 00:24:58 +0300

    Lowered timeout.

--- Configuration/FaviconDiscoveryPreferences.cs
+++ Configuration/FaviconDiscoveryPreferences.cs
@@ -5,6 +5,6 @@ namespace FaviconExtractor
     internal static class FaviconDiscoveryPreferences
     {
         public static readonly int[] PreferredIconSizes = new[] { 64, 32, 16 };
-        public static readonly TimeSpan HtmlRequestTimeout = TimeSpan.FromSeconds(12);
+        public static readonly TimeSpan HtmlRequestTimeout = TimeSpan.FromSeconds(6);
     }
 }
